d11f402ba7ac47bb5d6ebacaeae91dbe0ba13a37
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-07-09 18:14:56 +0300

    ...

--- Universal/DataAccess/IEntityRepository.cs
+++ Universal/DataAccess/IEntityRepository.cs
@@ -1,12 +1,12 @@
-﻿using Entities.Abstract;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Universal.Entities;
 
-namespace DataAccess.Abstract
+namespace Universal.DataAccess
 {
     #region Comment
     //Class means it is reference type and IEntity means either we can write IEntity or anything implemented from IEntity and new() means our entity can be gotten instance from it since IEntity is an interface that's why we cannot use it cause of we can use that we want to prevent user to write IEntity in generic type
--- Universal/Entities/IEntity.cs
+++ Universal/Entities/IEntity.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Abstract
+namespace Universal.Entities
 {
     #region Comment
     //If any classes inherited from IEntity means that they are database table
     #endregion
     public interface IEntity
     {
-
     }
 }
