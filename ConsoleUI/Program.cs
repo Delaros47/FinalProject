8b8b32e83972197c81aaa3c59685d10f907f11fc
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-07-09 14:59:35 +0300

    ...

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 
@@ -13,11 +14,35 @@ using DataAccess.Concrete.InMemory;
 
 
 
-ProductManager productManager = new ProductManager(new InMemoryProductDal());
-foreach (var product in productManager.GetAll())
+
+
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+foreach (var product in productManager.GetByUnitPrice(40,100))
 {
-    Console.WriteLine($"{product.ProductName,-10} {product.UnitPrice,10}");
+    Console.WriteLine($"{product.ProductName,-35} {product.UnitPrice,20}");
 }
+
+
+//ProductManager productManager = new ProductManager(new EfProductDal());
+//foreach (var product in productManager.GetAllByCategoryId(5))
+//{
+//    Console.WriteLine($"{product.ProductName,-35} {product.UnitPrice,20}");
+//}
+
+//ProductManager productManager = new ProductManager(new EfProductDal());
+//foreach (var product in productManager.GetAll())
+//{
+//    Console.WriteLine($"{product.ProductName,-35} {product.UnitPrice,20}");
+//}
+
+//ProductManager productManager = new ProductManager(new InMemoryProductDal());
+//foreach (var product in productManager.GetAll())
+//{
+//    Console.WriteLine($"{product.ProductName,-10} {product.UnitPrice,10}");
+//}
+
+
 
 
 
