1f03494a5a9bb25ef77843ed3e67713ab6a24896
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-07-18 22:56:35 +0300

    ...

---
+++ Universal/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
