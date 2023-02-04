14b71c2c421a34bc7f78e22506db81da83e76a68
Ahmet Hamdi <Anatolian47@hotmail.com>
2023-02-04 18:29:04 +0300

    ...

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,11 +8,18 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
 
-            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
+            //CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            //foreach (var category in categoryManager.GetAll())
+            //{
+            //    Console.WriteLine(category.CategoryName);
+            //}
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(category.CategoryName);
+                Console.WriteLine($"{product.ProductName,-30}{product.CategoryName,25}");
             }
+
 
             Console.ReadLine();
         }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework.Contexts;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,24 @@ using Universal.DataAccess.EntityFramework;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal:EfEntityRepositoryBase<Product,NortwindContext>,IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NortwindContext>, IProductDal
     {
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NortwindContext context = new NortwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = p.ProductId,
+                                 CategoryName = c.CategoryName,
+                                 ProductName = p.ProductName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
